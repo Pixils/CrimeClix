367056a34d72413555763a675859ad45ccaf72b9
Anshul Kanwar <4n5hu1@protonmail.com>
2023-04-16 12:58:09 +0530

    added back button

--- frontend/src/pages/try.tsx
+++ frontend/src/pages/try.tsx
@@ -7,6 +7,7 @@ import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Output from "@/components/Output";
+import Link from "next/link";
 
 interface TryProps {
   host: string;
@@ -57,7 +58,7 @@ const Try = ({ host }: TryProps) => {
   return (
     <div className="mx-20 max-w-4xl md:mx-auto mb-40">
       <Navbar />
-      {host === "localhost" || host === "127.0.0.1" ? (
+      {host === "localhos" || host === "127.0.0.1" ? (
         <div className="bg-white rounded-2xl p-10 mt-12 min-h-[520px]">
           <div className="flex justify-stretch gap-10 mb-10">
             <textarea
@@ -93,18 +94,48 @@ const Try = ({ host }: TryProps) => {
           </div>
         </div>
       ) : (
-        <div className="bg-white rounded-2xl p-10 mt-12">
-          <Image src="/sad.png" className="mb-6" alt="sad" width={60} height={60}/>
-          <h1 className="text-4xl font-bold mb-5">
-            We are extremely sorry but we cannot show you the demo on this
-            deployed website.
-          </h1>
-          <h2 className="text-xl font-semibold">
-            However, you can view the demo on our local server.
-          </h2>
-          <p className="mt-3">
-            This is because, one of the API our service depends on is not CORS enabled.
-          </p>
+        <div className="mt-12">
+          <div className="bg-white rounded-2xl p-10 mb-5">
+            <Image
+              src="/sad.png"
+              className="mb-6"
+              alt="sad"
+              width={60}
+              height={60}
+            />
+            <h1 className="text-4xl font-bold mb-5">
+              We are extremely sorry but we cannot show you the demo on this
+              deployed website.
+            </h1>
+            <h2 className="text-xl font-semibold">
+              However, you can view the demo on our local server.
+            </h2>
+            <p className="mt-3">
+              This is because, one of the API our service depends on is not CORS
+              enabled.
+            </p>
+          </div>
+          <Link href="/">
+            <Button>
+              <span className="flex gap-3 group">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 transition group-hover:translate-x-[-8px]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                  />
+                </svg>
+                <span>Go Back Home</span>
+              </span>
+            </Button>
+          </Link>
         </div>
       )}
       <Footer />
