6284ba3e11cbd25a827f93bed0a1ed732e55c296
Anshul Kanwar <4n5hu1@protonmail.com>
2023-04-16 07:41:52 +0530

    add IPFS upload

--- frontend/src/components/Output.tsx
+++ frontend/src/components/Output.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 
 interface OutputProps {
-  output: string[];
+  src: string | undefined;
   status: "idle" | "loading";
 }
 
-const Output = ({ output, status }: OutputProps) => {
+const Output = ({ src, status }: OutputProps) => {
   if (status === "loading") {
     return <h1>Loading...</h1>;
   }
 
-  if (status === "idle" && output.length === 0) {
-    return <div></div>
+  if (status === "idle" && !src) {
+    return <div></div>;
   }
 
   return (
-    <div className="grid grid-cols-3 gap-5 place-items-center h-full">
-      {output.map((src) => (
-        <Image key={src} src={src} className="rounded-3xl" alt="output" width={256} height={256} />
-      ))}
+    <div>
+      <Image
+        key={src}
+        src={src!}
+        className="rounded-3xl"
+        alt="output"
+        width={512}
+        height={512}
+      />
     </div>
   );
 };
--- frontend/src/pages/try.tsx
+++ frontend/src/pages/try.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Output from "@/components/Output";
 import axios from "axios";
@@ -6,33 +7,47 @@ import { useState } from "react";
 
 const Try = () => {
   const [prompt, setPrompt] = useState("");
-  const [output, setOutput] = useState<string[]>([]);
+  const [output, setOutput] = useState<string | undefined>(undefined);
   const [status, setStatus] = useState<"idle" | "loading">("idle");
+  const [uploadStatus, setUploadStatus] = useState(false);
 
   const text2Image = async (prompt: string) => {
-    console.log("Generating images");
+    console.log("Generating image");
     setStatus("loading");
+    setUploadStatus(false);
 
-    const updatedPrompt = `${prompt}, portrait, realistic, looking at the camera`
+    const updatedPrompt = `${prompt}, portrait, realistic, looking at the camera`;
 
     const {
       data: { output },
     } = await axios.post("https://stablediffusionapi.com/api/v3/text2img", {
       key: "c7dKHZ709uP1N6yRp1mS8F3ALK3CrldPfuIPfnwSz7rML1mqjiOPGQ1SiixI",
       prompt: updatedPrompt,
-      width: 256,
-      height: 256,
-      samples: 3,
+      width: 512,
+      height: 512,
+      samples: 1,
       promptStrenght: 20,
     });
 
-    console.log(output);
-    setOutput(output);
+    console.log(output[0]);
+    setOutput(output[0]);
     setStatus("idle");
   };
 
+  const uploadToIPFS = async () => {
+    try {
+      await axios.post("http://127.0.0.1:5000/portraits/", {
+        portrait: output,
+      });
+
+      setUploadStatus(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="mx-20 max-w-4xl md:mx-auto">
+    <div className="mx-20 max-w-4xl md:mx-auto mb-40">
       <Navbar />
       <div className="bg-white rounded-2xl p-10 mt-12">
         <div className="flex justify-stretch gap-10 mb-10">
@@ -45,11 +60,18 @@ const Try = () => {
             rows={10}
           ></textarea>
           <div>
-            <Output status={status} output={output} />
+            <Output status={status} src={output} />
           </div>
         </div>
-        <Button onClick={() => text2Image(prompt)}>Generate</Button>
+        <div className="flex gap-3 justify-between">
+          <Button onClick={() => text2Image(prompt)}>Generate</Button>
+          <span>
+            {uploadStatus && <span className="mr-5">Uploaded to IPFS</span>}
+            <Button onClick={() => uploadToIPFS()}>Next</Button>
+          </span>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
