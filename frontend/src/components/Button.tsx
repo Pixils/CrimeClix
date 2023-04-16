869753c24e74b8b921f631b642f82b54ad9572d5
Anshul Kanwar <4n5hu1@protonmail.com>
2023-04-16 06:26:54 +0530

    add stabeldiffusion

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,11 +1,11 @@
-import { Children } from "react";
-
 interface ButtonProps {
-  children: React.ReactNode
+  onClick?: () => void;
+  children: React.ReactNode;
 }
-const Button = ({ children }: ButtonProps) => {
+const Button = ({ onClick, children }: ButtonProps) => {
   return (
     <button
+      onClick={onClick}
       className="text-white px-12 py-3 font-lg font-semibold rounded-2xl bg-gradient-to-tr from-indigo-500 to-indigo-700 hover:from-indigo-500 hover:to-indigo-800 shadow-2xl shadow-stone-800/30 transition hover:scale-110"
     >
       {children}
--- frontend/src/pages/try.tsx
+++ frontend/src/pages/try.tsx
@@ -1,36 +1,54 @@
 import Button from "@/components/Button";
 import Navbar from "@/components/Navbar";
-import Link from "next/link";
+import Output from "@/components/Output";
+import axios from "axios";
+import { useState } from "react";
 
 const Try = () => {
+  const [prompt, setPrompt] = useState("");
+  const [output, setOutput] = useState<string[]>([]);
+  const [status, setStatus] = useState<"idle" | "loading">("idle");
+
+  const text2Image = async (prompt: string) => {
+    console.log("Generating images");
+    setStatus("loading");
+
+    const updatedPrompt = `${prompt}, portrait, realistic, looking at the camera`
+
+    const {
+      data: { output },
+    } = await axios.post("https://stablediffusionapi.com/api/v3/text2img", {
+      key: "c7dKHZ709uP1N6yRp1mS8F3ALK3CrldPfuIPfnwSz7rML1mqjiOPGQ1SiixI",
+      prompt: updatedPrompt,
+      width: 256,
+      height: 256,
+      samples: 3,
+      promptStrenght: 20,
+    });
+
+    console.log(output);
+    setOutput(output);
+    setStatus("idle");
+  };
+
   return (
     <div className="mx-20 max-w-4xl md:mx-auto">
       <Navbar />
-      <div className="grid place-items-center min-h-[calc(100vh-5rem-2.5rem)]">
-        <div className="flex flex-col gap-8 items-center">
-          <h1 className="text-6xl font-bold text-zinc-300">🚧 Coming Soon</h1>
-          <Link href="/" className="group">
-            <Button>
-              <span className="flex gap-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 transition group-hover:translate-x-[-12px]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                  />
-                </svg>
-                Go Back Home
-              </span>
-            </Button>
-          </Link>
+      <div className="bg-white rounded-2xl p-10 mt-12">
+        <div className="flex justify-stretch gap-10 mb-10">
+          <textarea
+            className="bg-zinc-100 p-10 rounded-xl"
+            placeholder="Enter your description of criminal here..."
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            cols={30}
+            rows={10}
+          ></textarea>
+          <div>
+            <Output status={status} output={output} />
+          </div>
         </div>
+        <Button onClick={() => text2Image(prompt)}>Generate</Button>
       </div>
     </div>
   );
